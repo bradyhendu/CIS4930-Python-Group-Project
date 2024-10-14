342add33479b0dd8e37faed55b3bbb3a5cebd17a
Brady Henderson <bradyhendu@gmail.com>
2024-10-14 14:48:07 -0400

    works on reading preference list

--- input.py
+++ input.py
@@ -1,6 +1,7 @@
-def read_ingredients():
+import json
+def read_ingredients(input_file):
     ingredients = []
-    with open('ingredients.csv', 'r') as f:
+    with open(input_file, 'r') as f:
         for line in f:
             #skip first line
             if 'name' in line:
@@ -16,3 +17,12 @@ def read_ingredients():
             #add to list of ingredients
             ingredients.append(ingredient)
     return ingredients
+
+def read_preferences(input_file):
+    preferences = []
+    with open(input_file, 'r') as f:
+        preferences = json.load(f)
+        
+        ##take the list of different user preferences in the dictionary and convert it to a list
+        preferences = preferences['user_preferences']
+    return preferences
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import input
 
 if __name__ == '__main__':
-    ingredient_list = input.read_ingredients()
-    print(ingredient_list)
+    ingredient_list = input.read_ingredients("ingredients.csv")
+    preference_list = input.read_preferences("preferences.json")
+    
+    #for testing
+    for ingredient in ingredient_list:
+        print(ingredient)
+    
+    for preference in preference_list:
+        print(preference)
     
