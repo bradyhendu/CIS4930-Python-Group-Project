69e0531409b3ff6e565a18b40b1a1e0d910b48bd
Brady Henderson <bradyhendu@gmail.com>
2024-11-05 16:07:19 -0500

    create shopping list is finished

--- main.py
+++ main.py
@@ -3,15 +3,31 @@ from recipes import recommend_recipes
 from output import write_output
 
 #outside imports
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def create_meal_plan(recommended_recipes, user_preferences):
     #Creates a meal plan based on the recommended recipes and user preferences
     pass
 
 def create_shopping_list(recommended_recipes, available_ingredients):
-    #Creates a shopping list based on the recommended recipes and available ingredients
-    pass
+    #Loop through the recommended recipes and create a shopping list based on the missing ingredients
+    available_ingredient_names = {ingredient["name"] for ingredient in available_ingredients}
+    shopping_list = []
+    for recipe in recommended_recipes:
+        for recipe_name, recipe_details in recipe.items():
+            for ingredient, quantity in recipe_details["ingredients"].items():
+                if ingredient not in available_ingredient_names:
+                    shopping_list.append({"ingredient": ingredient, "quantity": quantity})
+                if ingredient in available_ingredient_names:
+                    #If the ingredient is available, check if the quantity is sufficient
+                    available_quantity = int(next((item["quantity"] for item in available_ingredients if item["name"] == ingredient), 0))
+                    if available_quantity < quantity:
+                        shopping_list.append({"ingredient": ingredient, "quantity": quantity - available_quantity})
+                    #if the ingredient is already in the shopping list, update the quantity
+                    for item in shopping_list:
+                        if item["ingredient"] == ingredient:
+                            item["quantity"] += quantity
+    return shopping_list
 
 
 
@@ -28,6 +44,7 @@ if __name__ == '__main__':
     
     #Creates a shopping list based on the recommended recipes and available ingredients
     shopping_list = create_shopping_list(recommened_recipies, ingredient_list)
+    print(shopping_list)
     
     #Writes the meal plan and shopping list to a single output file
     write_output(meal_plan, shopping_list, "output.txt")
