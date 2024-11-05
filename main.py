8bd090bc30e806c097b3f63a104ec39a0e0b9aaf
Brady Henderson <bradyhendu@gmail.com>
2024-11-05 15:48:27 -0500

    Creates blank functions

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import input
 from recipes import recommend_recipes
+from output import write_output
+
+#outside imports
+import matplotlib.pyplot as plt
+
+def create_meal_plan(recommended_recipes, user_preferences):
+    #Creates a meal plan based on the recommended recipes and user preferences
+    pass
+
+def create_shopping_list(recommended_recipes, available_ingredients):
+    #Creates a shopping list based on the recommended recipes and available ingredients
+    pass
+
+
 
 if __name__ == '__main__':
     # Read the ingredients and preferences from the input files
@@ -10,10 +24,14 @@ if __name__ == '__main__':
     recommened_recipies = recommend_recipes(ingredient_list, preference_list)
     
     #Creates a meal plan based on the recommended recipes and user preferences
-    
+    meal_plan = create_meal_plan(recommened_recipies, preference_list)
     
     #Creates a shopping list based on the recommended recipes and available ingredients
+    shopping_list = create_shopping_list(recommened_recipies, ingredient_list)
     
     #Writes the meal plan and shopping list to a single output file
+    write_output(meal_plan, shopping_list, "output.txt")
+    
+    #visualize data
     
     
---
+++ output.py
@@ -0,0 +1,3 @@
+def write_output(meal_plan, shopping_list, output_file):
+    #Writes the meal plan and shopping list to a single output file
+    pass
